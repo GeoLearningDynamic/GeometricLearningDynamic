211e5449f318902efe3810097b0e8a8236cec796
GeoLearningDynamic <geo.dynam@outlook.com>
2026-03-09 02:53:55 +0200

    Create __init__.py

---
+++ geometric_learning /data /__init__.py
@@ -0,0 +1,11 @@
+from .manifold_dataset import ManifoldDataset
+from .sphere_data import SphereDataset
+from .torus_data import TorusDataset
+from .swiss_roll_data import SwissRollDataset
+
+__all__ = [
+    'ManifoldDataset',
+    'SphereDataset',
+    'TorusDataset',
+    'SwissRollDataset'
+]
